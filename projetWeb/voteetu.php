e7a0bc151da78abadb0b01bf577133d43fc0d4cf
Kodlack <34751983+Kodlack@users.noreply.github.com>
2018-10-13 18:05:58 +0200

    Add files via upload

--- projetWeb/voteetu.php
+++ projetWeb/voteetu.php
@@ -9,34 +9,69 @@
 
 
 <HTML>
+<h1>Bienvenue étudiant</h1>
+<p>Ici, vous pouvez voter pour chaque UE que vous avez suivis. Vos notes seront prises en compte par les professeurs concernés.</p>
+
+<form method= "POST" action="traitement.php">
 
 <fieldset>
-    <legend>Choisissez un UE</legend>
+    <legend>Math</legend>
 
-	<form action="">
-  <input type="radio" name="ue" value="math"> Math<br>
-  <input type="radio" name="ue" value="anglais"> Anglais<br>
-  <input type="radio" name="ue" value="prog"> Prog<br>
-  <input type="radio" name="ue" value="algo"> Algo<br>
-  <input type="radio" name="ue" value="eco"> Economie<br>
+		<input type="radio" name="math" value="tmecon" required> Très mécontent<br>
+		<input type="radio" name="math" value="mecon"> Mécontent<br>
+		<input type="radio" name="math" value="meh"> Moyen<br>
+		<input type="radio" name="math" value="satis"> Satisfait<br>
+		<input type="radio" name="math" value="tsatis"> Très satisfait<br>
+
+</fieldset>
+
+<fieldset>
+    <legend>Anglais</legend>
+
+		<input type="radio" name="anglais" value="tmecon" required> Très mécontent<br>
+		<input type="radio" name="anglais" value="mecon"> Mécontent<br>
+		<input type="radio" name="anglais" value="meh"> Moyen<br>
+		<input type="radio" name="anglais" value="satis"> Satisfait<br>
+		<input type="radio" name="anglais" value="tsatis"> Très satisfait<br>
+
+
+</fieldset>
+
+<fieldset>
+    <legend>Prog</legend>
+
+		<input type="radio" name="prog" value="tmecon" required> Très mécontent<br>
+		<input type="radio" name="prog" value="mecon"> Mécontent<br>
+		<input type="radio" name="prog" value="meh"> Moyen<br>
+		<input type="radio" name="prog" value="satis"> Satisfait<br>
+		<input type="radio" name="prog" value="tsatis"> Très satisfait<br>
+
+</fieldset>
+
+<fieldset>
+    <legend>Algo</legend>
+	
+		<input type="radio" name="algo" value="tmecon" required> Très mécontent<br>
+		<input type="radio" name="algo" value="mecon"> Mécontent<br>
+		<input type="radio" name="algo" value="meh"> Moyen<br>
+		<input type="radio" name="algo" value="satis"> Satisfait<br>
+		<input type="radio" name="algo" value="tsatis"> Très satisfait<br>
+
+</fieldset>
+
+<fieldset>
+    <legend>Economie</legend>
+
+		<input type="radio" name="eco" value="tmecon" required> Très mécontent<br>
+		<input type="radio" name="eco" value="mecon"> Mécontent<br>
+		<input type="radio" name="eco" value="meh"> Moyen<br>
+		<input type="radio" name="eco" value="satis"> Satisfait<br>
+		<input type="radio" name="eco" value="tsatis"> Très satisfait<br>
+
+</fieldset>
+<br><br>	
+<input type="submit" value="Envoyer" />
+<br><br>
 </form>
-
-</fieldset>
-
-<fieldset>
-    <legend>Choisissez la note à attribuer à l'UE choisit</legend>
-
-      <input type="radio" name="note" value="tmecon"> Très mécontent :'(<br>
-  <input type="radio" name="note" value="mecon"> Mécontent :(<br>
-  <input type="radio" name="note" value="meh"> Moyen :/<br>
-  <input type="radio" name="note" value="satis"> Satisfait :)<br>
-  <input type="radio" name="note" value="tsatis"> Très satisfait :D<br>
-
-</fieldset>
-
-
-
-
-
 
 </HTML>
