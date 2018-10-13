0a92be72bb4d7431f8426e2d6c020339d2dc7ea4
Kodlack <34751983+Kodlack@users.noreply.github.com>
2018-10-13 17:30:43 +0200

    Add files via upload

---
+++ projetWeb/voteetu.php
@@ -0,0 +1,42 @@
+<?php
+
+
+
+
+
+
+?>
+
+
+<HTML>
+
+<fieldset>
+    <legend>Choisissez un UE</legend>
+
+	<form action="">
+  <input type="radio" name="ue" value="math"> Math<br>
+  <input type="radio" name="ue" value="anglais"> Anglais<br>
+  <input type="radio" name="ue" value="prog"> Prog<br>
+  <input type="radio" name="ue" value="algo"> Algo<br>
+  <input type="radio" name="ue" value="eco"> Economie<br>
+</form>
+
+</fieldset>
+
+<fieldset>
+    <legend>Choisissez la note à attribuer à l'UE choisit</legend>
+
+      <input type="radio" name="note" value="tmecon"> Très mécontent :'(<br>
+  <input type="radio" name="note" value="mecon"> Mécontent :(<br>
+  <input type="radio" name="note" value="meh"> Moyen :/<br>
+  <input type="radio" name="note" value="satis"> Satisfait :)<br>
+  <input type="radio" name="note" value="tsatis"> Très satisfait :D<br>
+
+</fieldset>
+
+
+
+
+
+
+</HTML>
