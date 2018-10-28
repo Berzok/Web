97fbe8e91491bbb13bb15b3129afcc60e4821e90
Josh Hauser <33412940+npjoshh@users.noreply.github.com>
2018-10-28 13:25:10 +0100

    Add files via upload

--- projetWeb/etu.php
+++ projetWeb/etu.php
@@ -1,5 +1,10 @@
 <?php
+//Appel du script contenant des fonctions utilisées dans l'application
+require_once("fonctions.php");
 session_start();
+
+/*Si un utilisateur tente d'accéder à cette page sans être connecté ou sans avoir le rôle requis,
+il est redirigé vers la page d'accueil.*/
 if((!isset($_SESSION['login']) or !isset($_SESSION['role'])) or ($_SESSION['role'] != 'etu'))
 {
 	session_destroy();
@@ -7,6 +12,30 @@ if((!isset($_SESSION['login']) or !isset($_SESSION['role'])) or ($_SESSION['role
 }
 else
 {
-	require_once('header.php');
+	require_once("header.php");
 }
 ?>
+
+<html lang="fr">
+<head>
+	<title>Évaluation des enseigmenets</title>
+	<meta charset="UTF-8"/>
+	<link rel="stylesheet" href="votes.css"/>
+</head>
+<body>
+	<div id="container">
+		<div id="vote">
+			<?php
+			if(aVote($_SESSION['login']) == false)
+			{
+				require_once("voteetu.php");
+			}
+			else
+			{
+				echo "a voté"; //A modifier: à la place, on affiche les données du vote
+			}
+			?>
+		</div>
+	</div>
+</body>
+</html>
--- projetWeb/voteetu.php
+++ projetWeb/voteetu.php
@@ -1,69 +1,68 @@
-<?php
-?>
-<HTML>
-<h1>Bienvenue étudiant</h1>
-<p>Ici, vous pouvez voter pour chaque UE que vous avez suivis. Vos notes seront prises en compte par les professeurs concernés.</p>
-
-<form method= "POST" action="traitement.php">
-
-<fieldset>
-    <legend>Math</legend>
-
-		<input type="radio" name="math" value="tmecon" required> Très mécontent<br>
-		<input type="radio" name="math" value="mecon"> Mécontent<br>
-		<input type="radio" name="math" value="meh"> Moyen<br>
-		<input type="radio" name="math" value="satis"> Satisfait<br>
-		<input type="radio" name="math" value="tsatis"> Très satisfait<br>
-
-</fieldset>
-
-<fieldset>
-    <legend>Anglais</legend>
-
-		<input type="radio" name="anglais" value="tmecon" required> Très mécontent<br>
-		<input type="radio" name="anglais" value="mecon"> Mécontent<br>
-		<input type="radio" name="anglais" value="meh"> Moyen<br>
-		<input type="radio" name="anglais" value="satis"> Satisfait<br>
-		<input type="radio" name="anglais" value="tsatis"> Très satisfait<br>
-
-
-</fieldset>
-
-<fieldset>
-    <legend>Prog</legend>
-
-		<input type="radio" name="prog" value="tmecon" required> Très mécontent<br>
-		<input type="radio" name="prog" value="mecon"> Mécontent<br>
-		<input type="radio" name="prog" value="meh"> Moyen<br>
-		<input type="radio" name="prog" value="satis"> Satisfait<br>
-		<input type="radio" name="prog" value="tsatis"> Très satisfait<br>
-
-</fieldset>
-
-<fieldset>
-    <legend>Algo</legend>
+<html>
+<body>
+	<h1>Évaluation des enseigments</h1>
+	<p id="description">Ici, vous pouvez voter pour chaque UE que vous avez suivi. Vos votes seront pris en compte par les professeurs concernés.</p>
 	
-		<input type="radio" name="algo" value="tmecon" required> Très mécontent<br>
-		<input type="radio" name="algo" value="mecon"> Mécontent<br>
-		<input type="radio" name="algo" value="meh"> Moyen<br>
-		<input type="radio" name="algo" value="satis"> Satisfait<br>
-		<input type="radio" name="algo" value="tsatis"> Très satisfait<br>
+	<br>
 
-</fieldset>
+	<form method= "POST" action="traitement.php">
+		<fieldset>
+		    <legend>Mathématiques</legend>
 
-<fieldset>
-    <legend>Economie</legend>
+				<input type="radio" name="math" value="tmecon" required> Très mécontent<br>
+				<input type="radio" name="math" value="mecon"> Mécontent<br>
+				<input type="radio" name="math" value="meh"> Moyen<br>
+				<input type="radio" name="math" value="satis"> Satisfait<br>
+				<input type="radio" name="math" value="tsatis"> Très satisfait<br>
 
-		<input type="radio" name="eco" value="tmecon" required> Très mécontent<br>
-		<input type="radio" name="eco" value="mecon"> Mécontent<br>
-		<input type="radio" name="eco" value="meh"> Moyen<br>
-		<input type="radio" name="eco" value="satis"> Satisfait<br>
-		<input type="radio" name="eco" value="tsatis"> Très satisfait<br>
+		</fieldset>
 
-</fieldset>
-<br><br>	
-<input type="submit" value="Envoyer" />
-<br><br>
-</form>
+		<fieldset>
+		    <legend>Anglais</legend>
 
-</HTML>
+				<input type="radio" name="anglais" value="tmecon" required> Très mécontent<br>
+				<input type="radio" name="anglais" value="mecon"> Mécontent<br>
+				<input type="radio" name="anglais" value="meh"> Moyen<br>
+				<input type="radio" name="anglais" value="satis"> Satisfait<br>
+				<input type="radio" name="anglais" value="tsatis"> Très satisfait<br>
+
+
+		</fieldset>
+
+		<fieldset>
+		    <legend>Programmation</legend>
+
+				<input type="radio" name="prog" value="tmecon" required> Très mécontent<br>
+				<input type="radio" name="prog" value="mecon"> Mécontent<br>
+				<input type="radio" name="prog" value="meh"> Moyen<br>
+				<input type="radio" name="prog" value="satis"> Satisfait<br>
+				<input type="radio" name="prog" value="tsatis"> Très satisfait<br>
+
+		</fieldset>
+
+		<fieldset>
+		    <legend>Algorithmique</legend>
+			
+				<input type="radio" name="algo" value="tmecon" required> Très mécontent<br>
+				<input type="radio" name="algo" value="mecon"> Mécontent<br>
+				<input type="radio" name="algo" value="meh"> Moyen<br>
+				<input type="radio" name="algo" value="satis"> Satisfait<br>
+				<input type="radio" name="algo" value="tsatis"> Très satisfait<br>
+
+		</fieldset>
+
+		<fieldset>
+		    <legend>Economie</legend>
+
+				<input type="radio" name="eco" value="tmecon" required> Très mécontent<br/>
+				<input type="radio" name="eco" value="mecon"> Mécontent<br>
+				<input type="radio" name="eco" value="meh"> Moyen<br>
+				<input type="radio" name="eco" value="satis"> Satisfait<br>
+				<input type="radio" name="eco" value="tsatis"> Très satisfait<br>
+		</fieldset>
+		<br>
+		<input type="submit" value="Envoyer" />
+		<br>
+	</form>
+</body>
+</html>
